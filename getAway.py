9cd37390d7a612a5740644b0d14e7c1e6f4b43eb
ApoorvaUplap <apoorvauplap@gmail.com>
2024-05-25 14:40:33 -0700

    Add files via upload

---
+++ getAway.py
@@ -0,0 +1,226 @@
+from random import shuffle
+from random import randint
+import math
+from copy import copy, deepcopy
+
+from cards import Hand, Deck
+from players import BasePolicy1, BasePolicy2, HumanPlayer, RandomAgent, Rollout, HumanLikeCPU
+from qlearn import QLearner
+from advanced_players import BasePolicy
+
+class GetAwayGame:
+    ACE_OF_SPADES = ('Spades', 'A', 12)
+    MAX_GAMES = 100000
+
+    def __init__(self, all_players, loser_count):
+        ''' players: list of players
+            loser_count: empty dictionary which keeps score '''
+        self.all_players = all_players
+        self.loser_count = loser_count
+
+    def run_game(self, show_every_round, show_cpu_cards):
+        ''' all complicated game logic occurs here '''
+        self.deal_cards()
+        players = copy(self.all_players)
+
+        # shuffling list of players so that their order
+        # in the game rotation is randomized
+        shuffle(players)
+
+        # game info variables:
+        garbage = Hand([])
+        table_cards = Hand([])
+        starter = -1
+        num_live_players = len(players)
+
+        winners = []
+        # Initializing game: first round decides who starts
+        i=0
+        while i<num_live_players:
+            # First move of game - player plays ace of spades
+            if i<1:
+                for player in players:
+                    if player.hand.has_specific_card(self.ACE_OF_SPADES):
+                        starter = players.index( player )
+                        table_cards.pick_up_card(
+                            player.hand.pop_card(self.ACE_OF_SPADES)
+                        )
+                        i+=1
+            # remaining players play their ace after first player
+            else:
+                # next player in rotation plays
+
+                table_cards.pick_up_card(
+                    players[(starter + i) % num_live_players].getAway_move(
+                        table_cards, garbage, players, i, starter, True, winners, num_live_players
+                    )
+                )
+                i+=1
+
+        # dump all cards after first round
+        garbage.pick_up_cards( table_cards.pop_all_cards() )
+
+        # central loop for game
+        j=1
+        while num_live_players>1:
+            # begin a single round
+            round_end_early = False
+            i=0
+            next_starting_player = players[starter]
+            while i<num_live_players and (not round_end_early):
+
+                # player takes turn
+                current = players[(starter + i) % num_live_players]
+                if show_cpu_cards:
+                    if not isinstance(current, HumanPlayer):
+                        current.show_cards()
+
+                if (j>1) and isinstance(current, QLearner):
+                    current.update_qtable()
+                table_cards.pick_up_card( current.getAway_move(table_cards, garbage, players, i, starter, False, winners, num_live_players) )
+
+                # checking game status
+                if not (i==0):
+                    if table_cards.top_card_is_diffsuit():
+                        round_end_early = True
+                    elif table_cards.top_card_is_highest():
+                        # checking if player who just played threw the highest card
+                        next_starting_player = current
+
+                if show_every_round:
+                    self.print_status(current, table_cards)
+
+                i+=1
+            # loop breaks once everyone played, or round ended early (pick up)
+
+            if round_end_early:
+                if show_every_round:
+                    print(f"{next_starting_player.name} picks up the table cards!")
+
+                next_starting_player.pick_up_cards(
+                    table_cards.pop_all_cards()
+                )
+
+            else:
+                garbage.pick_up_cards( table_cards.pop_all_cards() )
+
+            # case where next_starting_player must pick from garbage since
+            # they start the next round
+            if next_starting_player.hand.get_card_count() < 1:
+                if show_every_round:
+                    print(str(next_starting_player.name)+" picks a card from"
+                          +" the garbage since they threw highest but are out!")
+
+                garbage.shuffle_cards()
+                next_starting_player.pick_up_card( garbage.pop_card(1) )
+
+            # removing winners:
+            next_round_players = []
+            for player in players:
+                if player.hand.get_card_count() >= 1:
+                    next_round_players.append(player)
+                else:
+                    winners.append(player)
+
+            if show_every_round:
+                print("End of Round status: ")
+                for player in players:
+                    print( f"{player.name} - {player.hand.get_card_count()}"
+                           + " cards" )
+
+            players.clear()
+            players = next_round_players
+            num_live_players = len(players)
+
+            # setting up starting player for next round
+            starter = players.index( next_starting_player )
+            j+=1
+
+        # at end of game, update loser count
+        LOSER = players[0].name
+        if LOSER in self.loser_count:
+            self.loser_count[LOSER] += 1
+        else:
+            self.loser_count[LOSER] = 1
+
+    def print_status(self, just_played, table_cards):
+        ''' print status of game
+            takes player who most recently played, garbage, and table cards
+            to display status
+        '''
+        print(f"Most recent turn: {just_played.name}")
+        print(f"table: ")
+        table_cards.show_cards()
+        print("----------------------------------------")
+
+    def run_multiple_games(self, show_every_round, show_cpu_cards, count):
+        if (count <= self.MAX_GAMES):
+            for i in range(count):
+                self.run_game(show_every_round, show_cpu_cards)
+        else:
+            print("Mumber of games must be less than " + str(self.MAX_GAMES))
+
+        for player in self.all_players:
+            if isinstance(player, QLearner):
+                player.save_qtable()
+
+    def deal_cards(self):
+        split_decks = Deck().deal(len(self.all_players))
+        i=0
+        for player in self.all_players:
+            player.set_hand( split_decks[i] )
+            i+=1
+
+
+# ---------------------------------------------------------------------------
+
+def get_players():
+    players = [None for x in range(4)]
+    names = ["Agent1", "Agent2", "Agent3", "Agent4"]
+    for i in range(4):
+        selection = -1
+        while selection not in [1,2,3,4]:
+            print("Select the type for player "+str(i+1))
+            print(" 1 - RandomAgent\n 2 - Heuristic1\n 3 - Heuristic2\n"+ 
+                  " 4 - Human\n")
+            selection = int(input())
+
+        name = ""
+        while len(name) not in list(range(2,30)):
+            print("Type the players name or leave blank (Agent"+str(i+1)+" default)")
+            name = str(input())
+            if len(name)==0:
+                name = names[i]
+
+        names[i] = name
+
+        if selection==1:
+            players[i] = RandomAgent(name=names[i])
+        elif selection==2:
+            players[i] = BasePolicy1(name=names[i])
+        elif selection==3:
+            players[i] = BasePolicy2(name=names[i])
+        else:
+            players[i] = HumanPlayer(name=names[i])
+    return players
+
+if __name__ == '__main__':
+    # train_ai()
+    all_players = get_players()
+    loser_count = {}
+    game = GetAwayGame( all_players, loser_count )
+
+    interactive = False
+    for player in all_players:
+        if isinstance(player, HumanPlayer):
+            interactive = True
+
+    if interactive:
+        game.run_game(True, False)
+
+    else:
+        rounds = int(input("How many games would you like to run (up to 100000): "))
+        game.run_multiple_games(False, False, rounds)
+
+    for name in loser_count:
+        print(name + " couldn't get away " + str(loser_count[name]) + " time(s)!")
---
+++ players.py
@@ -0,0 +1,361 @@
+from abc import ABC, abstractmethod
+from cards import Hand
+from random import randint, shuffle
+from copy import copy, deepcopy
+
+class Player(ABC):
+
+    def __init__(self, name, hand=[]):
+        ''' Given name, and hand (list of card tuples), initializes player '''
+        self.hand = Hand(hand)
+        self.name = name
+        self.rounds_played = 0
+
+    def set_hand(self, hand):
+        self.hand = Hand(hand)
+
+    def show_cards(self):
+        print(str(self.name))
+        self.hand.show_cards()
+
+    def pick_up_cards(self, c):
+        self.hand.pick_up_cards(c)
+
+    def pick_up_card(self, c):
+        self.hand.pick_up_card(c)
+
+    def getAway_move(self, table_cards, garbage, players, i, starter, isFirstRound, winners, num_live_players = 0):
+        ''' abstract method. table_cards is a Hand
+            containing cards on the table,
+
+            * function should return the card played by the player
+            * it should be a valid move depending on the table cards.
+        '''
+        pass
+
+class HumanPlayer(Player):
+    ''' human player which interacts with game through command line '''
+    def getAway_move(self, table_cards, garbage, players, i, starter, isFirstRound, winners, num_live_players = 0):
+        selection = 0
+        self.hand.cards = sorted(self.hand.cards, key=lambda element: (element[0], element[2]))
+        print("Would you like to see your cards? (y/n)")
+        show_player_cards = input()
+        if ( show_player_cards.lower() == 'y' ):
+            self.hand.show_cards()
+
+        if table_cards.get_card_count() > 0:
+            table_suit = table_cards.get_bottom_suit()
+            if self.hand.has_suit( table_suit ):
+                # Case 1 - player has the live suit
+                valid_move = False
+                while not valid_move:
+                    print("(Suited) Enter the index of the"
+                    + " card you would like to play: ")
+                    selection = int(input())
+                    valid_move = self.hand.get_card_suit(selection)==table_suit
+            else:
+                # Case 2 - player does not have live suit
+                valid_moves = list(range(1, (self.hand.get_card_count()+1)))
+                while not (selection in valid_moves):
+                    print("(Any) Enter the index of the"
+                    + " card you would like to play: ")
+                    selection = int(input())
+        else:
+            # Case 3 - player is starting round
+            valid_moves = list(range(1, (self.hand.get_card_count()+1)))
+            while not (selection in valid_moves):
+                print("(Start of Round) Enter the index of the"
+                + " card you would like to play: ")
+                selection = int(input())
+
+        return self.hand.pop_card(selection)
+    
+class BasePolicy1(Player):
+    def getAway_move(self, table_cards, garbage, players, i, starter, isFirstRound, winners, num_live_players = 0):
+        selection = 0
+        if table_cards.get_card_count() > 0:
+            table_suit = table_cards.get_bottom_suit()
+            if self.hand.has_suit( table_suit ):
+                # Case 1 - player has the live suit
+                card_values = [ card[2] if card[0]==table_suit else -1
+                                for card in self.hand.cards ]
+                selection = card_values.index(min(card_values))+1
+            else:
+                # Case 2 - player does not have live suit
+                card_values = [ card[2] for card in self.hand.cards ]
+                selection = card_values.index(max(card_values))+1
+        else:
+            # Case 3 - player is starting a round
+            card_values = [ card[2] for card in self.hand.cards ]
+            selection = card_values.index(min(card_values))+1
+
+        return self.hand.pop_card( selection )
+
+
+class BasePolicy2(Player):
+    def getAway_move(self, table_cards, garbage, players, i, starter, isFirstRound, winners, num_live_players = 0):
+        selection = 0
+        if table_cards.get_card_count() > 0:
+            table_suit = table_cards.get_bottom_suit()
+            if self.hand.has_suit( table_suit ):
+                if len(table_cards.cards) == num_live_players-1:
+                    card_values = [ card[2] if card[0]==table_suit else -1
+                                for card in self.hand.cards ]
+                    selection = card_values.index(max(card_values))+1 
+                # Case 1 - player has the live suit
+                else:
+                    selection = 0
+                    card_values = [ card[2] if card[0]==table_suit else -1
+                                for card in self.hand.cards ]
+                    table_cards_val = [ card[2] if card[0]==table_suit else -1
+                                for card in table_cards.cards ]
+                    maxCard = max(table_cards_val)
+                    while card_values[selection] == -1:
+                        selection += 1
+                    for i in range(len(card_values)):
+                        if card_values[i] > card_values[selection] and card_values[i] < maxCard:
+                            selection = i
+                    selection += 1
+            else:
+                # Case 2 - player does not have live suit
+                card_values = [ card[2] for card in self.hand.cards ]
+                selection = card_values.index(max(card_values))+1
+        else:
+            # Case 3 - player is starting a round
+            card_values = [ card[2] for card in self.hand.cards ]
+            selection = card_values.index(min(card_values))+1
+
+        return self.hand.pop_card( selection )
+
+class RandomAgent(Player):
+    ''' CPU player which selects cards at random to play '''
+    def getAway_move(self, table_cards, garbage, players, i, starter, isFirstRound, winners, num_live_players = 0):
+        if self.hand.get_card_count() == 1:
+            return self.hand.pop_card(1)
+        selection = 0
+        # note: randint includes end points, range() does not
+        if table_cards.get_card_count() > 0:
+            table_suit = table_cards.get_bottom_suit()
+
+            if self.hand.has_suit( table_suit ):
+                # Case 1 - player has the live suit
+                self.hand.shuffle_cards()
+                end = self.hand.get_card_count()+1
+                for i in range(1,  end):
+                    if self.hand.get_card_suit(i) == table_suit:
+                        selection = i
+                        i = end
+            else:
+                # Case 2 - player does not have live suit
+                selection = randint(1, self.hand.get_card_count())
+        else:
+            # Case 3 - player is starting round (any)
+            selection = randint(1, self.hand.get_card_count())
+
+        return self.hand.pop_card( selection )
+    
+
+class HumanLikeCPU(Player):
+    ''' human-like CPU - Always selects highest possible card
+    Removed this entirely'''
+    def getAway_move(self, table_cards, num_live_players = 0):
+        selection = 0
+        if table_cards.get_card_count() > 0:
+            table_suit = table_cards.get_bottom_suit()
+            if self.hand.has_suit( table_suit ):
+                # Case 1 - player has the live suit
+                card_values = [ card[2] if card[0]==table_suit else -1
+                                for card in self.hand.cards ]
+                selection = card_values.index(max(card_values))+1
+            else:
+                # Case 2 - player does not have live suit
+                card_values = [ card[2] for card in self.hand.cards ]
+                selection = card_values.index(max(card_values))+1
+        else:
+            # Case 3 - player is starting a round
+            card_values = [ card[2] for card in self.hand.cards ]
+            selection = card_values.index(max(card_values))+1
+
+        return self.hand.pop_card( selection )
+
+class Rollout(Player):
+
+    def getAway_move(self, table_cards, garbage, players, i, starter, isFirstRound, winners, num_live_players = 0):
+        playersCopy = deepcopy(players)
+        table_cards_copy = deepcopy(table_cards)
+        garbage_copy = deepcopy(garbage)
+        winners_copy = deepcopy(winners)
+        basePolicy1 = BasePolicy1(self.name, deepcopy(self.hand.cards))
+        basePolicy2 = BasePolicy2(self.name, deepcopy(self.hand.cards))
+        playersCopy[(starter+i)%num_live_players] = deepcopy(basePolicy1)
+        score1 = runRoundsAndCalculatePoints(table_cards_copy, garbage_copy, playersCopy, i, starter, isFirstRound, winners_copy, num_live_players)
+        playersCopy = deepcopy(players)
+        playersCopy[(starter+i)%num_live_players] = deepcopy(basePolicy2)
+        table_cards_copy = deepcopy(table_cards)
+        garbage_copy = deepcopy(garbage)
+        winners_copy = deepcopy(winners)
+        score2 = runRoundsAndCalculatePoints(table_cards_copy, garbage_copy, playersCopy, i, starter, isFirstRound, winners_copy, num_live_players)
+        if score1 <= score2:
+            card = basePolicy1.getAway_move(table_cards, garbage, players, i, starter, isFirstRound, winners, num_live_players)
+            return self.hand.pop_card(card)
+        card = basePolicy2.getAway_move(table_cards, garbage, players, i, starter, isFirstRound, winners, num_live_players)
+        return self.hand.pop_card(card)
+
+
+
+def runRoundsAndCalculatePoints(table_cards, garbage, players, i, starter, isFirstRound, winners, num_live_players):
+    playerName = players[(starter + i) % num_live_players].name
+    if isFirstRound:
+        while i<num_live_players:
+            # next player in rotation plays
+            table_cards.pick_up_card(
+                players[(starter + i) % num_live_players].getAway_move(
+                    table_cards, garbage, players, i, starter, isFirstRound, winners, num_live_players
+                )
+            )
+            i+=1
+        
+        round = 2
+        # dump all cards after first round
+        garbage.pick_up_cards( table_cards.pop_all_cards() )
+
+        # central loop for game
+        j=1
+        while num_live_players>1 and round < 4:
+            # begin a single round
+            round_end_early = False
+            i=0
+            next_starting_player = players[starter]
+            while i<num_live_players and (not round_end_early):
+
+                # player takes turn
+                current = players[(starter + i) % num_live_players]
+
+                table_cards.pick_up_card( current.getAway_move(table_cards, garbage, players, i, starter, isFirstRound, winners, num_live_players) )
+
+                # checking game status
+                if not (i==0):
+                    if table_cards.top_card_is_diffsuit():
+                        round_end_early = True
+                    elif table_cards.top_card_is_highest():
+                        # checking if player who just played threw the highest card
+                        next_starting_player = current
+
+                
+                i+=1
+            # loop breaks once everyone played, or round ended early (pick up)
+
+            if round_end_early:
+
+                next_starting_player.pick_up_cards(
+                    table_cards.pop_all_cards()
+                )
+
+            else:
+                garbage.pick_up_cards( table_cards.pop_all_cards() )
+
+            # case where next_starting_player must pick from garbage since
+            # they start the next round
+            if next_starting_player.hand.get_card_count() < 1:
+
+                garbage.shuffle_cards()
+                next_starting_player.pick_up_card( garbage.pop_card(1) )
+
+            # removing winners:
+            next_round_players = []
+            for player in players:
+                if player.hand.get_card_count() >= 1:
+                    next_round_players.append(player)
+                else:
+                    winners.append(player)
+
+
+            players.clear()
+            players = next_round_players
+            num_live_players = len(players)
+
+            # setting up starting player for next round
+            starter = players.index( next_starting_player )
+            j+=1
+            round += 1
+        
+        for player in players:
+            if player.name == playerName:
+                card_values = [ card[2]
+                                for card in player.hand.cards]
+                return sum(card_values)
+        return 0
+    else:
+        round = 1
+
+        # central loop for game
+        j=1
+        while num_live_players>1 and round < 4:
+            # begin a single round
+            round_end_early = False
+            next_starting_player = players[starter]
+            while i<num_live_players and (not round_end_early):
+
+                # player takes turn
+                current = players[(starter + i) % num_live_players]
+
+                table_cards.pick_up_card( current.getAway_move(table_cards, garbage, players, i, starter, isFirstRound, winners, num_live_players) )
+
+                # checking game status
+                if not (i==0):
+                    if table_cards.top_card_is_diffsuit():
+                        round_end_early = True
+                    elif table_cards.top_card_is_highest():
+                        # checking if player who just played threw the highest card
+                        next_starting_player = current
+
+                
+                i+=1
+            # loop breaks once everyone played, or round ended early (pick up)
+
+            if round_end_early:
+
+                next_starting_player.pick_up_cards(
+                    table_cards.pop_all_cards()
+                )
+
+            else:
+                garbage.pick_up_cards( table_cards.pop_all_cards() )
+
+            # case where next_starting_player must pick from garbage since
+            # they start the next round
+            if next_starting_player.hand.get_card_count() < 1:
+
+                garbage.shuffle_cards()
+                next_starting_player.pick_up_card( garbage.pop_card(1) )
+
+            # removing winners:
+            next_round_players = []
+            for player in players:
+                if player.hand.get_card_count() >= 1:
+                    next_round_players.append(player)
+                else:
+                    winners.append(player)
+
+
+            players.clear()
+            players = next_round_players
+            num_live_players = len(players)
+
+            # setting up starting player for next round
+            starter = players.index( next_starting_player )
+            i = 0
+            j+=1
+            round += 1
+        
+        for player in players:
+            if player.name == playerName:
+                card_values = [ card[2]
+                                for card in player.hand.cards]
+                return sum(card_values)
+        return 0
+
+
+        
+
+        
